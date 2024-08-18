e439238bed972de8aa91364340497e0d23a0f8bf
TheAliBigdeli <bigdeli.ali3@gmail.com>
2024-08-18 22:06:32 +0330

    added a few changes

--- dockerfiles/prod/django/entrypoint.sh
+++ dockerfiles/prod/django/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+echo "Makeing the migrations"
+python manage.py migrate
+
+echo "Collecting statics"
 python manage.py collectstatic --no-input
 
+echo "stating wsgi gunicorn server"
 gunicorn core.wsgi:application --bind "0.0.0.0:8000" --daemon
 
+
+echo "starting nginx server"
 nginx -g 'daemon off;'
